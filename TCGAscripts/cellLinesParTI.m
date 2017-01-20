d1d0157d4eb32bf8cc2bbc92dc608e576df100a7
Jean Hausser <jean.hausser@weizmann.ac.il>
2017-01-20 17:30:32 +0200

    Added two scripts to analyze the effect of mutations, CNAs and do ParTI on the cell lines dataset of Beretina 2012

---
+++ TCGAscripts/cellLinesParTI.m
@@ -0,0 +1,176 @@
+%% ParTI pipeline for TCGA datasets
+addpath ParTI
+origPath = pwd;
+
+% nArchetypes = 4;
+% global ForceNArchetypes; ForceNArchetypes = nArchetypes;
+
+% Load the data into Matlab from a comma separated value (CSV) file
+% The file is a purely numerical matrix, with patients as rows and genes as
+% columns
+geneExpression = dlmread('expMatrix.csv', ',');
+% The file is formated as samples (i.e. patients) x genes. 
+% We load gene names.
+geneNames = importdata('geneListExp.list');
+
+%% We expand the sample attributes by computing changes in GO category expression
+% This section is optional. It makes it possible to determine broad gene 
+% expression categories that are over-expressed in the vicinity of 
+% archetypes. This is helpful to characterize the archetypes.
+[GOExpression,GONames,~,GOcat2Genes] = MakeGOMatrix(geneExpression, geneNames, ...
+                {'../ParTI/MSigDB/c2.cp.v4.0.symbols.gmt', '../ParTI/MSigDB/c5.all.v4.0.symbols.gmt'}, ...
+                20);
+
+% GOExpression is a matrix of 2106 patients x 162 GO categories, and
+% GONames contains the name of the GO categories.
+% GOcat2Genes is a boolean matrix of genes x GO categories which
+% indicates, for each category, which genes were used to compute it.
+% In the next line, we expand this matrix so that it has as many columns as
+% the number of continuous features (clinical + GO). Because clinical
+% features are typically not directly based on specific genes, we add
+% zeroes in the corresponding columns:
+%GOcat2Genes=[zeros(size(GOcat2Genes,1),size(contAttr,2)),GOcat2Genes];
+
+% We won't expand the continuous clinical features with GO-based continuous
+% features but instead examine them in a separate analysis
+%contAttrNames = [contAttrNames, GONames];
+%contAttr = [contAttr, GOExpression];
+
+%% We import the sample attributes, i.e. the clinical data on patients
+% These come in two kinds: 
+% - discrete attributes, i.e. categorical data (citizenship, gender, cancer progression grade, ...)
+% - continuous attributes, i.e. numerical data (weight, age, tumor volume, ...)
+% We start by loading discrete attributes.
+[discrAttrNames, discrAttr] = ...
+    read_enriched_csv('discreteClinicalData_reOrdered.tsv', char(9));
+%where discrAttr is a matrix of 2106 patients x 25 attributes. The names of
+%the attributes are stored in discrAttrNames.
+
+%Load continuous features
+%[contAttrNames, contAttr] = ...
+%   read_enriched_csv('continuousClinicalData_reOrdered.tsv', char(9));
+if exist('continuousClinicalData_reOrdered_justData.csv', 'file') == 2
+    contAttr = dlmread('continuousClinicalData_reOrdered_justData.csv', ',');
+    contAttrNames = importdata('continuousClinicalData_reOrdered_featNames.list');
+    contAttrNames = regexprep(contAttrNames, '_', ' ');
+else
+    contAttr = [];
+    contAttrNames = [];
+end
+
+%% Finally, we substitute underscores '_' in variable names with spaces ' ' 
+% to prevent the characters following underscores from appearing in indice
+% position.
+discrAttrNames = regexprep(discrAttrNames, '_', ' ');
+GONames = regexprep(GONames, '_', ' ');
+
+%% We are now ready to perform Pareto Task Inference.
+% We use the Sisal algorithm (1), with up to 8 dimensions. We provide the
+% discrete patient attributes, and ask ParTI to preliminary booleanize these
+% attributes (0). We also pass continuous patient features. We pass a boolean 
+% matrix specifiying which genes each continuous feature is baesd on (to be used
+% in the leave-one-out procedure). 
+% We specify that the enrichment analysis will be performed with a bin size 
+% of 5%. Finally, the output of the the analysis will be stored in an
+% Comma-Separated-Value text file, under the name 'Cancer_enrichmentAnalysis_*.csv'.
+
+cd ParTI
+if exist(strcat(origPath, '/arcs_dims.tsv'), 'file') == 2
+    fprintf('Reloading previously computed archetypes\n');
+    load(strcat(origPath, '/arcs_dims.tsv'))
+    arc = arcs_dims;
+    load(strcat(origPath, '/arcsOrig_genes.tsv'))
+    arcOrig = arcsOrig_genes;
+else
+    [arc, arcOrig, ~] = ParTI_lite(geneExpression);
+    save(strcat(origPath, '/arcs_dims.tsv'), 'arc', '-ascii')
+    save(strcat(origPath, '/arcsOrig_genes.tsv'), 'arcOrig', '-ascii')
+end
+
+%% Clinical features
+close all
+ParTI_lite(geneExpression, 1, 10, discrAttrNames, ...
+    discrAttr, 0, contAttrNames, contAttr, [], 0.15, ...
+    strcat(origPath, '/clinicalEnrichment'), arcOrig);
+
+%% MSigDB gene groups
+%clear discrAttr contAttr;
+close all
+ParTI_lite(geneExpression, 1, 10, [], [], ...
+    0, GONames, GOExpression, [], 0.15, ...
+    strcat(origPath, '/MSigDBenrichment'), arcOrig);
+
+%% Genes highly expression at each archetype
+%clear discrAttr contAttr;
+close all
+ParTI_lite(geneExpression, 1, 10, [], [], ...
+    0, geneNames, geneExpression, [], 0.15, ...
+    strcat(origPath, '/genesEnrichment'), arcOrig);
+
+%% Repeat, removing a tissue type
+tissues = {'lung', 'central nervous system', 'skin', 'large intestine'};
+i = 1;
+for i = 1:length(tissues)
+    myt = upper(tissues{i});
+    tmp = discrAttr(:,1);
+    sel = find(~strcmp(tmp, myt));
+    
+    tGeneExpression = geneExpression(sel,:);
+    tDiscrAttr = discrAttr(sel,:);
+    tContAttr = contAttr(sel,:);
+    tGOExpression = GOExpression(sel,:);
+    
+    close all;
+    [arc, arcOrig, ~] = ParTI_lite(tGeneExpression);
+    ParTI_lite(tGeneExpression, 1, 10, discrAttrNames, ...
+                tDiscrAttr, 0, contAttrNames, tContAttr, [], 0.15, ...
+                strcat(origPath, '/clinicalEnrichment_no', myt), arcOrig);
+    
+    ParTI_lite(tGeneExpression, 1, 10, [], [], ...
+                0, GONames, tGOExpression, [], 0.15, ...
+                strcat(origPath, '/MSigDBenrichment_no', myt), arcOrig);
+end
+
+%% Mutations
+clear GOExpression GONames GOcat2Genes;
+
+mut = dlmread(strcat(origPath, '/mutMatrix_reOrdered_booleanized_justData.csv'), ',');
+mutNames = importdata(strcat(origPath, '/mutMatrix_reOrdered_booleanized_geneNames.list'));
+
+% Project mutations to gene groups
+posMut = find(cellfun('length', regexp(mutNames, '=1$')') > 0);
+[GOmut,GOmutNames,~,GOcat2mutGenes] = MakeGOMatrix(mut(:,posMut), strrep(mutNames(posMut), '=1', ''), ...
+                {'MSigDB/c2.cp.v4.0.symbols.gmt', 'MSigDB/c5.all.v4.0.symbols.gmt'}, ...
+                5);
+
+close all
+ParTI_lite(geneExpression, 1, ForceNArchetypes, mutNames, ...
+    mut, -1, [], [], [], 0.2, ...
+    strcat(origPath, '/mutEnrichment'), arcOrig);
+
+close all
+ParTI_lite(geneExpression, 1, ForceNArchetypes, [], ...
+    [], [], GOmutNames, GOmut, [], 0.1, ...
+    strcat(origPath, '/MSigDBmutEnrichment'), arcOrig);
+clear GOmut GOmutNames GOcat2mutGenes;
+
+%% Copy number alterations
+clear mut mutNames pc;
+
+cop = dlmread(strcat(origPath, '/copMatrix_reOrdered_booleanized_justData.csv'), ',');
+copNames = importdata(strcat(origPath, '/copMatrix_reOrdered_booleanized_geneNames.list'));
+
+close all
+ParTI_lite(geneExpression, 1, ForceNArchetypes, copNames, cop, ...
+    0, [], [], [], 0.1, ...
+    strcat(origPath, '/cnvEnrichment'), arcOrig);
+
+
+%% Finally, we perform the compete analysis, including randomization
+% controls and archetype error estimation.
+% close all
+[arc, arcOrig, pc] = ParTI(geneExpression);
+
+%[arc, arcOrig, pc, errs, pval] = ParTI(geneExpression, 1, 8, discrAttrNames, ...
+%    discrAttr, 0, GONames, GOExpression, GOcat2Genes, 0.1, ...
+%    strcat(origPath, '/clinicalMSigDBpValEnrichment'));
---
+++ TCGAscripts/learnMutEffects.R
@@ -0,0 +1,417 @@
+rm(list=ls()); gc();
+
+geneNames <- read.table("geneListExp.list", as.is=T, h=F)[,1]
+geneNamesFilt <- read.table("geneNamesAfterExprFiltering.list", as.is=T, h=F)[,1]
+geneExpression <- read.csv("expMatrix.csv", as.is=T, h=F)
+colnames(geneExpression) <- geneNames;
+rm(geneNames);
+tmp <- sapply(geneNamesFilt, function(g) {
+    geneExpression[,g]
+})
+geneExpression <- tmp
+
+mutations <- read.csv("mutMatrix_reOrdered_booleanized_justData.csv",
+                      as.is=T, h=F)
+mutGeneNames <-
+    read.table("mutMatrix_reOrdered_booleanized_geneNames.list",
+               h=F, as.is=T)[,1]
+colnames(mutations) <- mutGeneNames;
+rm(mutGeneNames)
+dim(mutations)
+NaNcol <- grep("=NaN", colnames(mutations));
+isHealthy <- as.logical(apply(mutations[,NaNcol], 1, mean))
+mutations <- mutations[,setdiff(1:ncol(mutations), NaNcol)]
+rm(NaNcol)
+
+CNAs <- read.csv("copMatrix_reOrdered_booleanized_justData.csv",
+                 as.is=T, h=F)
+copGeneNames <-
+    read.table("copMatrix_reOrdered_booleanized_geneNames.list",
+               h=F, as.is=T)[,1]
+x <- copGeneNames[1]
+copTab <- t(sapply(copGeneNames, function(x) {
+    strsplit(x, "=")[[1]]
+}))
+CNAgenes <- 
+    as.character(unlist(
+        sapply(
+            read.csv("/home/jhausser/work/cancerTaskAtlas/Ciriello2013_ng.2762-S2.csv",
+                     as.is=T)[,"Genes.in.GISTIC.peak"],
+            function(x) {
+                strsplit(x, ",")[[1]]
+            })))
+CNAgenes <- unique(CNAgenes)
+x <- CNAgenes[1]
+sel <- sort(unique(unlist(sapply(CNAgenes, function(x) {
+    grep(sub("$", "=", x), copGeneNames)
+}))))
+CNAsSmall <- CNAs[,sel]
+colnames(CNAsSmall) <- copGeneNames[sel]
+plot(rev(sort(apply(CNAsSmall, 2, mean)))); abline(v=600, lty=2)
+colnames(CNAsSmall) <- sprintf("%s_CNA", colnames(CNAsSmall));
+rm(copGeneNames)
+CNAs <- CNAsSmall;
+rm(CNAsSmall)
+## CNAs <- CNAs[,rev(order(apply(CNAs, 2, mean)))]
+## plot(apply(CNAs, 2, mean)); abline(v=4000, lty=2)
+## CNAs <- CNAs[,1:4000]
+
+healthyProfile <- apply(geneExpression[isHealthy,], 2, median)
+
+gE0 <- apply(geneExpression, 1, function(x) { x - mean(x) })
+rm(geneExpression)
+## gE00 <- apply(gE0, 1, function(x) { x - mean(x) })
+gE00 <- apply(gE0, 2, function(x) { x - healthyProfile } )
+rm(gE0); gc();
+
+library(MASS)
+M <- t(mutations)
+rm(mutations)
+Mp <- rbind(M, t(CNAs))
+## Mp <- M
+Mp <- t(CNAs);
+
+rm(CNAs, M)
+E <- gE00
+rm(gE00)
+
+## save.image("learnMutEffects.rda");
+rm(list=ls()); gc();
+load("learnMutEffects.rda");
+
+## 50 is the tikhonov factor (lambda)
+## X <-
+##     t(
+##     (solve( M %*% t(M) + 50 * diag(1, nrow=dim(M)[1]) ) ) %*%
+##     M %*% t(E)
+##     )
+X <-
+    t(
+    (solve( Mp %*% t(Mp) + 50 * diag(1, nrow=dim(Mp)[1]) ) ) %*%
+    Mp %*% t(E)
+    )
+hist(X, seq(-max(abs(X)), max(abs(X)), len=40))
+hist(apply(X, 2, sd), 20)
+
+x <- rownames(Mp)[1]
+Xd <-
+    sapply(## rownames(Mp)
+        1:nrow(Mp), function(x) {
+            ## cat(sprintf("%s\n", x))
+            apply(E[,as.logical(Mp[x,])], 1, mean) -
+                apply(E[,!as.logical(Mp[x,])], 1, mean)
+        })
+Xd <- mcapply(Mp, 1, function(x) {
+    sel <- as.logical(x)
+    if ( sum(sel) < 10 ) { return(rep(NA, nrow(E))) }
+    cat(sprintf("%d\n", sum(sel)))
+    apply(E[,sel], 1, mean) - apply(E[,!sel], 1, mean)
+})
+Xd[1:5,1:5]
+## save(Xd, file="Xdelta.rda");
+load("Xdelta.rda");
+## These are the mutations with strongest impact on gene expression
+rev(sort(apply(X, 2, sd)))[1:10]
+
+library(ade4)
+dudi1 <- dudi.pca(t(X), scale=F, center=F, scannf=F, nf=3)
+cumsum(dudi1$eig)[1:5] / sum(dudi1$eig)
+library(rgl)
+plot3d(dudi1$li)
+
+mutWithStrongestEffect <-
+    names(rev(sort(apply(dudi1$li[,2:3]^2, 1, sum)))[1:8])
+## mutWithStrongestEffect <-
+##     names(rev(sort(apply(dudi1$li[,2:3]^2, 1, sum)))[1:2])
+dudi1$li[mutWithStrongestEffect,2:3]
+
+## first axis is healthy -> cancer axis
+pdf("mutShiftsPCA.pdf", height=4, width=4)
+plot(dudi1$li[,2:3],
+     xlim=range(dudi1$li[,2]) * 1.1,
+     ylim=range(dudi1$li[,3]) * 1.1)
+## text(dudi1$li[mutWithStrongestEffect,2:3],
+##      sub("=1", "", mutWithStrongestEffect), pos=3)
+s.arrow(dudi1$li[mutWithStrongestEffect,2:3],
+        label=sub("=1$", "", mutWithStrongestEffect), add.plot=T)
+dev.off();
+rm(dudi1)
+
+dudi2 <- dudi.pca(t(E), scale=F, scannf=F, nf=3)
+
+## Xproj <- t(X) %*% as.matrix(dudi2$co) %*%
+##     diag(1 / apply(dudi2$co, 2, function(x) { (sum(x^2)) }))
+
+avgE <- apply(E, 1, mean)
+## Compute the end points of mutations
+## X <- cbind(X, CTRL=avgE)
+
+## save.image("learnMutEffects2.rda")
+## load("learnMutEffects2.rda")
+
+Xproj <-
+    t(apply(X, 2, function(x) { x - avgE })) %*%
+    as.matrix(dudi2$c1)
+## Xproj <-
+##     t(apply(Xd, 2, function(x) { x - avgE })) %*%
+##     as.matrix(dudi2$c1)
+
+## Norms of the PCs:
+## apply(dudi2$c1, 2, function(x) { sqrt(sum(x^2)) }) ## 1, good
+Xproj[mutWithStrongestEffect,]
+## XbackProj <- as.matrix(dudi2$co) %*% t(Xproj)
+## tmp <- as.matrix(dudi2$c1) %*% t(Xproj)
+## XbackProj <- apply(tmp, 2, function(x) { x + avgE })
+XbackProj <- as.matrix(dudi2$c1) %*% t( t(X) %*% as.matrix(dudi2$c1) )
+plot(X[,mutWithStrongestEffect[1]], XbackProj[,mutWithStrongestEffect[1]]);
+plot(X[,mutWithStrongestEffect[1]] - mean(X[,mutWithStrongestEffect[1]]),
+     XbackProj[,mutWithStrongestEffect[1]]);
+abline(0,1, col="grey");
+plot(X[,1], XbackProj[,1]);
+abline(0,1, col="grey");
+
+plot(X[,1] - mean(X[,1]), XbackProj[,1]);
+abline(0,1, col="grey");
+
+sum(((X[,1] - mean(X[,1])) - XbackProj[,1]) * XbackProj[,1])
+sum(((X[,mutWithStrongestEffect[1]] - mean(X[,mutWithStrongestEffect[1]])) - XbackProj[,mutWithStrongestEffect[1]]) * XbackProj[,mutWithStrongestEffect[1]])
+
+res <- sapply(colnames(X), function(i) {
+    ##residuals: SS off Pareto front
+    sum(((X[,i] - mean(X[,i])) - XbackProj[,i])^2)
+})
+onf <- sapply(colnames(X), function(i) {
+    #SS on Parto front
+    sum(XbackProj[,i]^2)
+})
+    
+rev(sort(onf/res))[1:500]
+ratios <- log10(onf/res);
+rm(onf); rm(res);
+
+## Randomize mutations and see what the onf/res ratio is for random
+## mutations
+SSlist <-
+    sapply(1:20, function(i) {
+        Mr <- t(apply(Mp, 1, sample))
+        Xr <-
+            t(
+            (solve( Mr %*% t(Mr) + 50 * diag(1, nrow=dim(Mr)[1]) ) ) %*%
+            Mr %*% t(E)
+            )
+
+        ## Xrproj <-
+        ##     t(apply(Xr, 2, function(x) { x - avgE })) %*%
+        ##     as.matrix(dudi2$c1)
+        XrbackProj <- as.matrix(dudi2$c1) %*% t( t(Xr) %*% as.matrix(dudi2$c1) )
+
+        resr <- sapply(colnames(X), function(i) {
+            ##residuals: SS off Pareto front
+            sum(((Xr[,i] - mean(Xr[,i])) - XrbackProj[,i])^2)
+        })
+        onfr <- sapply(colnames(X), function(i) {
+            ##SS on Parto front
+            sum(XrbackProj[,i]^2)
+        })
+        return(list(resr=resr, onfr=onfr))
+    })
+onfr <- unlist(SSlist["onfr",])
+resr <- unlist(SSlist["resr",])
+rratios <- log10(onfr/resr);
+
+pVals <- sort(sapply(ratios, function(x) {
+    sum(x < log10(onfr/resr)) / length(onfr)
+}))
+
+mybreaks <- seq(min(c(ratios, rratios)), max(c(ratios, rratios)), len=20);
+pdf("ratiosSSonSSoff.pdf", height=4, width=4)
+hist(ratios,
+     breaks=mybreaks, main="",
+     col="lightblue", freq=F, ylim=c(0,1.25),
+     xlab=expression(paste(log[10], " (SSon / SSoff)")))
+legend("topleft", c("measured", "shuffled"),
+       fill=c("lightblue", "white"), col=c("black", "red"))
+hist(log10(onfr/resr), breaks=mybreaks, border="red",
+     col=NULL, add=T, freq=F)
+sapply(names(pVals[pVals<.01 | pVals>.99]), function(x) {
+    text(ratios[x], .3, sub("=1", "", x), srt=90)
+    arrows(ratios[x], .13, ratios[x], .02, len=.07)
+})
+dev.off();
+
+## These genes mutate outside the Pareto front:
+pVals[pVals>.999]
+## Protocadherin Fat 4, aka FAT4 tumor suppressor; inhibits YAP1
+
+sort(pVals[pVals<.001]) 
+## TP53 and GATA3 mutate inside the cancer space significantly more
+## than random mutations.
+## TP53 = Basal [2] (IntCl 2) DNA replication, Chromatin, patients 6 yrs
+## older patients, many tumor cells and necrosis, Infiltrating Ductal Carcinoma
+## GATA3 = LumB [3] (IntCl 3,4) ER+, Peroxisome (Mucinous Carcinoma?)
+
+## CDH1 = unchar archetype [1]: Infiltrating Lobular Carcinoma, Complete
+## Response, Locoregional Recurrence, postoperative radiotherapy, new
+## tumor event after initial treatment: NO, ER+/PR+, people not
+## included in 2012 study; translation / respiration, proteasome
+
+arcsOrig <- read.table("arcsOrig_genes_tabSep.tsv", sep="\t", h=F, as.is=T)[,-1]
+plot(as.numeric(arcsOrig[4,]), as.numeric(healthyProfile)); abline(0,1,col="grey")
+
+## genesFilt <- read.table("geneNamesAfterExprFiltering.list", as.is=T)[,1]
+## colnames(arcsOrig) <- genesFilt;
+
+apply(arcsOrig, 1, length)
+archProj <- t(sapply(1:4, function(i) {
+    m0 <- arcsOrig[i,] - healthyProfile - avgE
+    as.matrix(m0) %*% as.matrix(dudi2$c1)
+}))
+
+hPt <-
+    (- matrix(apply(E, 1, mean), nrow=1) ) %*% as.matrix(dudi2$co) %*%
+    diag(1 / apply(dudi2$co, 2, function(x) { sqrt(sum(x^2)) }))
+
+s <- 3;
+s <- 10;
+
+pdf("mutVectors.pdf", height=6*2, width=6);
+par(mfrow=c(2,1))
+plot(dudi2$li[,1:2], col="grey", pch=20,
+     xlim=range(archProj[,1]), ylim=range(archProj[,2]))
+points(archProj[,1:2], col="red", cex=3)
+text(archProj[,1], archProj[,2], 1:4)
+points(dudi2$li[isHealthy,1:2], col="green")
+## pH <- apply(dudi2$li[isHealthy,], 2, median)
+## points(pH[1], pH[2], pch=20, col="red", cex=2)
+
+points(hPt[1], hPt[2], col="green", pch=20, cex=2)
+sapply(mutWithStrongestEffect, function(x) {
+    v <- c(Xproj[x,1] - hPt[1], Xproj[x,2] - hPt[2]);
+    arrows(hPt[1], hPt[2],
+           hPt[1] + s * v[1], hPt[2] + s * v[2],
+           len=.1)
+    text(hPt[1] + s * v[1], hPt[2] + s * v[2],
+         sub("=1", "", x), pos=4)
+})
+
+legend("bottomleft", "healthy", pch=1, col="green");
+
+plot(dudi2$li[,c(1,3)], col="grey", pch=20,
+     xlim=range(archProj[,1]), ylim=range(archProj[,3]))
+points(archProj[,c(1,3)], col="red", cex=3)
+text(archProj[,1], archProj[,3], 1:4)
+points(dudi2$li[isHealthy,c(1,3)], col="green")
+## pH <- apply(dudi2$li[isHealthy,], 2, median)
+## points(pH[1], pH[2], pch=20, col="red", cex=2)
+
+hPt <-
+    (- matrix(apply(E, 1, mean), nrow=1) ) %*% as.matrix(dudi2$co) %*%
+    diag(1 / apply(dudi2$co, 2, function(x) { sqrt(sum(x^2)) }))
+points(hPt[1], hPt[3], col="green", pch=20, cex=2)
+x <- mutWithStrongestEffect[1]
+sapply(mutWithStrongestEffect, function(x) {
+    v <- c(Xproj[x,1] - hPt[1], Xproj[x,2] - hPt[2], Xproj[x,3] - hPt[3]);
+    arrows(hPt[1], hPt[3],
+           hPt[1] + s * v[1], hPt[3] + s * v[3],
+           len=.1)
+    text(hPt[1] + s * v[1], hPt[3] + s * v[3],
+         sub("=1", "", x), pos=4)
+})
+dev.off();
+
+hPt
+
+
+plot3d(dudi2$li, alpha=.35, col="grey", box=F)
+spheres3d(hPt, col="green", radius=5)
+spheres3d(archProj, col="blue", radius=5)
+text3d(archProj[,1], archProj[,2], archProj[,3], 1:4, adj=2.5)
+
+sapply(1:3, function(i) {
+    sapply(seq(i+1, 4), function(j) {
+        segments3d(archProj[c(i,j),1], archProj[c(i,j),2],
+                   archProj[c(i,j),3], col="blue", alpha=.5)
+    })
+})
+
+## Add mutations
+sapply(mutWithStrongestEffect, function(m) {
+    x <- Xproj[m,1:3]
+    v <- x - hPt;
+    
+    lines3d(c(hPt[1], s * v[1] + hPt[1]),
+            c(hPt[2], s * v[2] + hPt[2]),
+            c(hPt[3], s * v[3] + hPt[3]), col="black", lwd=2)
+    text3d(s * v[1] + hPt[1], s * v[2] + hPt[2],
+           s * v[3] + hPt[3], sub("_CNA$", "", sub("=1$", "", m)), adj=c(.5, 0))
+})
+
+##################################################
+
+## Show location of Integrative Clusters -> could they be species?
+
+## discClin <-
+##     read.table("discreteClinicalData_reOrdered_withTreatment.tsv",
+##                sep="\t", h=T)
+## summary(discClin[,7:9])
+
+IntClustAnnot <- read.table("TCGAIntClust1100.txt", sep="\t", h=T, as.is=T)
+rownames(IntClustAnnot) <- IntClustAnnot[,1]
+patientIDs <- read.table("patientIDs.list", h=F, as.is=T)[,1]
+ICs <- sapply(patientIDs, function(x) { IntClustAnnot[x,"IntClust"] })
+ICsL <- sort(unique(ICs)); ICsL <- ICsL[!is.na(ICsL)]
+
+
+
+plot(dudi2$li[,1:2], col="grey")
+
+pdf("mutVectorsICs.pdf", height=6*2, width=6);
+par(mfrow=c(2,1))
+plot(dudi2$li[,1:2], col="grey", pch=20,
+     xlim=range(archProj[,1]), ylim=range(archProj[,2]))
+points(archProj[,1:2], col="red", cex=3)
+text(archProj[,1], archProj[,2], 1:4)
+## points(dudi2$li[isHealthy,1:2], col="green")
+## pH <- apply(dudi2$li[isHealthy,], 2, median)
+## points(pH[1], pH[2], pch=20, col="red", cex=2)
+sapply(ICsL, function(IC) {
+    points(dudi2$li[ICs == IC,1:2], col=IC, pch=20)
+})
+
+plot(dudi2$li[,c(1,3)], col="grey", pch=20,
+     xlim=range(archProj[,1]), ylim=range(archProj[,3]))
+points(archProj[,c(1,3)], col="red", cex=3)
+text(archProj[,1], archProj[,3], 1:4)
+sapply(ICsL, function(IC) {
+    points(dudi2$li[ICs == IC,c(1,3)], col=IC, pch=20)
+})
+
+legend("topright", legend=ICsL, col=ICsL, pch=20)
+dev.off();
+
+##
+
+plot3d(dudi2$li, alpha=.35, col=ICs)
+plot3d(dudi2$li, alpha=.35, col="grey", box=F)
+## spheres3d(hPt, col="green", radius=5)
+spheres3d(archProj, col="blue", radius=5)
+text3d(archProj[,1], archProj[,2], archProj[,3], 1:4, adj=2.5)
+
+sapply(1:3, function(i) {
+    sapply(seq(i+1, 4), function(j) {
+        segments3d(archProj[c(i,j),1], archProj[c(i,j),2],
+                   archProj[c(i,j),3], col="blue", alpha=.5)
+    })
+})
+
+IC <- ICsL[1]
+sapply(ICsL, function(IC) {
+    ## plot3d(ellipse3d(cov(dudi2$li[IC == ICs,], use="pairwise.complete"),
+    ##                  centre=c(0,0,0), level=0.95), add=T)
+    midPoint <- apply(dudi2$li[IC == ICs,], 2, function(x) { mean(x, na.rm=T) })
+    plot3d(
+        ellipse3d(cov(dudi2$li[IC == ICs,], use="pairwise.complete"),
+                  centre=midPoint, level=0.66, col=IC, alpha=.1),
+        add=T)
+    text3d(midPoint[1], midPoint[2], midPoint[3], IC)
+})
